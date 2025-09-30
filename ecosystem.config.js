8a2266f39797e6fcface37a3a8a36ea64f99c7fe
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-09-30 19:24:49 -0300

    timestamp nos logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,10 @@ module.exports = {
       name: 'alerta-bitcoin',
       script: './alertaBitcoin.js',
       cron_restart: '*/2 * * * *', 
-      watch: false
+      watch: false,                 
+      instances: 1,                 
+      autorestart: false,           
+      log_date_format: 'YYYY-MM-DD HH:mm:ss'
     }
   ]
 };
